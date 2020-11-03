2c470f098116604cfcb3459073a6a81cc567e2fa
PURVA MEHTA <73185840+purvamehta2007@users.noreply.github.com>
2020-11-03 21:28:56 +0530

    Add files via upload

---
+++ biodata_dictionary.py
@@ -0,0 +1,12 @@
+biodata = {
+    'biodat':{
+        'name':'purva',
+        'course':'python',
+        'mobile':'9820345260',
+        'email':'purvamehta2007@gmail.com',
+    }
+}
+    
+    
+print('\nname:{}\ncourse:{}\nmobile:{}\nemail:{}.'.format(biodata['biodat']['name'], biodata['biodat']['course'], biodata['biodat']['mobile'], biodata['biodat']['email']))
+
